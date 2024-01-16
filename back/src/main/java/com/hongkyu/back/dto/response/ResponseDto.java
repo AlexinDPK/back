92189952840ffcf9147ec2ff07602b68531fa680
hongkyuRyu <minsubrother@naver.com>
2024-01-17 03:24:37 +0900

    2024-01-17

---
+++ back/src/main/java/com/hongkyu/back/dto/response/ResponseDto.java
@@ -0,0 +1,23 @@
+package com.hongkyu.back.dto.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.hongkyu.back.common.ResponseCode;
+import com.hongkyu.back.common.ResponseMessage;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDto {
+
+    private String code;
+    private String message;
+
+    public static ResponseEntity<ResponseDto> databaseError() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.DATEBASE_ERROR, ResponseMessage.DATEBASE_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+    }
+}
