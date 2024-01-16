593dcf12b4d13764bc30bc34d76f3bd419fb1061
hongkyuRyu <minsubrother@naver.com>
2024-01-16 18:06:02 +0900

    first commit

---
+++ back/src/test/java/com/hongkyu/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongkyu.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
